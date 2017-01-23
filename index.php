1eef573d0f24bac65056630a930dbb0bc98cbbf7
John <john.s.xerri@gmail.com>
2017-01-23 22:27:34 +0100

    Posibilitte de recup des balises recurente sans class

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 			<!-- Row milieu - colonne gauche -->
 			<article class="contenuToCheck">
 
-				<!-- CONTENU FACTICE (ici le site s'affichera) -->
+				<!-- CONTENU FACTICE DE TEST (ici le site s'affichera) -->
 					<div class="annonces">
 						<h2 class="titre2">Lorem ipsum dolor sit amet <span>Annonce Premium</span></h2>
 						<a class="lien2" href="#" title="La réponse D" >Réponse D</a>
@@ -38,11 +38,18 @@
 
 					<?php $img = array('https://scontent-ams3-1.xx.fbcdn.net/v/t31.0-8/10575158_783553525040027_6967185626754443436_o.jpg?oh=b5f193e0863ac38449b25e9b133b45a8&oe=591039A9','https://scontent-ams3-1.xx.fbcdn.net/v/t31.0-8/10003652_783553528373360_5849515899304776083_o.jpg?oh=a91c7b6000a216ab306a7f8126462dc4&oe=592388CE','https://scontent-ams3-1.xx.fbcdn.net/v/t31.0-8/1614306_783553511706695_6092934285515763786_o.jpg?oh=75078d493b14ebd8e42ce31799364544&oe=5909F1CE'); ?>
 					<?php $link = array('la reponse A', 'la reponse B', 'la reponse C'); ?>
+
 					<?php for ($i=0; $i < 3; $i++) { ?>
 						<div class="annonces">
-							<h2 class="titre">Lorem ipsum dolor sit amet <span>Annonce n. <?= $i; ?></span></h2>
-							<a class="lien" href="#" title="<?= $link[$i] ?>" ><?= $link[$i] ?></a>
-							<img class="image" src="<?= $img[$i] ?>" alt="<?= $link[$i] ?>" width="150" height="100" />
+							<h2 class="titre">Lorem ipsum dolor sit amet <span>Annonce n. <?= $i; ?></span> <span>test <?= $i; ?></span></h2>
+							<a class="lien" href="#<?= $i; ?>" title="<?= $link[$i] ?>" ><?= $link[$i] ?></a>
+							<hr />
+							<div class="img">
+								<img src="<?= $img[$i] ?>" alt="<?= $link[$i] ?>" title="balise img sans class dans une div parent" width="150" height="100" />
+								<a href="#<?= $i; ?>" title="<?= $link[$i] ?>" ><?= $link[$i] ?></a>
+							</div>
+							<hr />
+							<img class="image" src="<?= $img[$i] ?>" alt="<?= $link[$i] ?>" title="balise img avec class" width="150" height="100" />
 							<p class="description"><?= $i; ?> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</p>
 						</div>
 					<?php } ?>
--- js/script.js
+++ js/script.js
@@ -52,7 +52,7 @@ $(document).ready(function(){
 			//   }
 			// );
 
-			$( '.contenuToCheck ' + balise ).click(function(){
+			$( '.contenuToCheck ' + balise ).click(function(event){
 
 				// Si aucune instance n est lancer
 				if (i == 0) {
@@ -64,11 +64,23 @@ $(document).ready(function(){
 					if ( className == undefined ) {
 
 						className = $( this ).parent().attr('class');
+						// et on repere l endroit exact du click en cas de multiple balise identique sans .class
+						var occurenceBalise = $( this ).parent().children().text( event.tagName );
+						// On compte le nombre de balise
+						var nthChild = occurenceBalise.length;
+						// On boucle le nbr total de balise jusqu a tomber sur la position cliqué
+						for (var posBalise = 0; posBalise < nthChild; posBalise++) {
+							if (occurenceBalise['context'] == occurenceBalise[posBalise]) {
+								// On ajoute +1 pour utiliser le selecteur css ":nth-child" qui commence a 1 et non 0
+								var nthChild2 = posBalise+1;
+							}
+						}
+						
 						// Et pour s y retrouver on creer une variable Parent
 						var classNameParent = 1;
 
 						// ajout de la couleur pour le visuelle du click
-						$('.'+className).children().addClass('baliseSelection');
+						$('.'+className).find(balise+':nth-child('+nthChild2+')').addClass('baliseSelection');
 
 					// Si il y a une class presente
 					} else {
@@ -104,10 +116,27 @@ $(document).ready(function(){
 						if ( classNameParent == 1 ) {
 							// et pour chaque ligne on recupere la balise cliqué dans le parent qui contient une
 							// .class (en plus c'est non-greedy ^_^)
-							var regexBalise = new RegExp('<'+balise+'>([\\s\\S]*?)</'+balise+'>', 'g');
-							baliseHtmlContenu = baliseHtmlContenu.match( regexBalise );
+
+							// regex balise double
+							// var regexBaliseDouble = new RegExp('<'+balise+'>([\\s\\S]*?)</'+balise+'>', 'g');
+							// // regex balise simple
+							// var regexBaliseSimple = new RegExp('<'+balise+'([\\s\\S]*?)>', 'g');
+
+							// // Si la balise sans .class est double (<span></span> ..) :
+							// if ( baliseHtmlContenu.match( regexBaliseDouble ) ) {
+							// 	baliseHtmlContenu = baliseHtmlContenu.match( regexBaliseDouble );
+							// // Sinon c est une balise simple (<img /> ..) :
+							// } else {
+							// 	baliseHtmlContenu = baliseHtmlContenu.match( regexBaliseSimple );
+							// }
+
+							// /!\ plus tard : rechercher récurence entre les mots /!\
+							baliseHtmlContenu = $('.'+className).eq(j).find(balise+':nth-child('+nthChild2+')')[0].outerHTML;
+							// on efface la class pour l affichage
+							baliseHtmlContenu = baliseHtmlContenu.replace(' class="baliseSelection"','');
 							// on push la reponse dans le tableau HTML
 							tableauHtmlContenu.push( baliseHtmlContenu );
+
 						} else {
 							// on efface la class pour l affichage
 							baliseHtmlContenu = baliseHtmlContenu.replace(' baliseSelection','');
@@ -123,10 +152,10 @@ $(document).ready(function(){
 						// SI on a recuperer la class Parent
 						if ( classNameParent == 1 ) {
 							// On remplace baliseTextContenu par le text de l enfant de la .class
-							baliseTextContenu = $('.contenuToCheck .'+className ).eq(j).children().text();
+							baliseTextContenu = $('.contenuToCheck .'+className ).eq(j).find(balise).text();
 						}
 						// SI aucun text n est récupéré
-						if ( baliseTextContenu == '' ) {
+						if ( baliseTextContenu.trim() == '' ) {
 							baliseTextContenu = 'Aucun text';
 						} 
 						// on push la reponse dans le tableau Text
@@ -135,9 +164,21 @@ $(document).ready(function(){
 
 						// Recuperation du Href (lien page) ou Src (lien image) :
 						if (balise == 'img') {
-							var baliseHrefContenu = $('.contenuToCheck .'+className ).eq(j).attr('src');
+							// Si c est une image sans .class
+							if (classNameParent == 1) {
+								var baliseHrefContenu = $('.contenuToCheck .'+className ).eq(j).find(balise).attr('src');
+							// Si c est une image avec .class
+							} else {
+								var baliseHrefContenu = $('.contenuToCheck .'+className ).eq(j).attr('src');
+							}
 						} else {
-							var baliseHrefContenu = $('.contenuToCheck .'+className ).eq(j).attr('href');
+							// Si c est un lien sans .class
+							if (classNameParent == 1) {
+								var baliseHrefContenu = $('.contenuToCheck .'+className ).eq(j).find(balise).attr('href');
+							// Si c est un lien avec .class
+							} else {
+								var baliseHrefContenu = $('.contenuToCheck .'+className ).eq(j).attr('href');
+							}
 						}
 
 						// SI aucun lien n est récupéré
@@ -147,6 +188,13 @@ $(document).ready(function(){
 						// on push la reponse dans le tableau Href/Src
 						tableauHrefContenu.push( baliseHrefContenu );
 						// =====================================================
+					}
+
+					// Petit plus pour savoir qu une balise a ete recuperé sans class :
+					if (classNameParent == 1) {
+						className = 'sans .class récupéré';
+					} else {
+						className = 'avec la classe ' +className;
 					}
 
 					// Superbe affichage des résultats dans la console en attendant le .append() ^_^ :
@@ -161,7 +209,7 @@ $(document).ready(function(){
 					console.table(tableauHrefContenu);
 					console.log('%c----------------------------------','color:purple');
 					// on affiche la reponse
-					console.log('il y a ' +nbrClasse+ ' objet(s) recurent avec la classe ' +className+ '! ');
+					console.log('il y a ' +nbrClasse+ ' objet(s) recurent ' +className+ ' sur la balise ' +balise+ ' ! ');
 					console.log('%c===============================================','color:blue');
 					i++;
 
